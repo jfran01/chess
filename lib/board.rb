9b5a1c7ed6a8fc2e832a472756494be1c0f5f5c2
Juliette <211356692+jfran01@users.noreply.github.com>
2026-07-30 21:14:29 +0100

    change keys from symbols to arrays

--- lib/board.rb
+++ lib/board.rb
@@ -29,14 +29,18 @@ class Board
     piece.legal_move?(to_coord)
   end
 
+  def read_board
+    init_board
+  end
+
   private
 
   def init_board
     board = {}
-    X_AXIS.each do |x_coord|
-      Y_AXIS.each do |y_coord|
-        coord = y_coord + x_coord.to_s
-        board[coord.to_sym] = nil
+    8.downto(1) do |y|
+      8.downto(1) do |x|
+        coord = [x, y]
+        board[coord] = nil
       end
     end
     board
@@ -44,34 +48,7 @@ class Board
 
   def pop_board(icon)
     board = init_board
-    board.each_key do |coord|
-      case coord
-      when /\w2/
-        board[coord] = Pawn.new(:white, icon, coord)
-      when :a1, :h1
-        board[coord] = Rook.new(:white, icon, coord)
-      when :b1, :g1
-        board[coord] = Knight.new(:white, icon, coord)
-      when :c1, :f1
-        board[coord] = Bishop.new(:white, icon, coord)
-      when :d1
-        board[coord] = Queen.new(:white, icon, coord)
-      when :e1
-        board[coord] = King.new(:white, icon, coord)
-      when /\w7/
-        board[coord] = Pawn.new(:black, icon, coord)
-      when :a8, :h8
-        board[coord] = Rook.new(:black, icon, coord)
-      when :b8, :g8
-        board[coord] = Knight.new(:black, icon, coord)
-      when :c8, :f8
-        board[coord] = Bishop.new(:black, icon, coord)
-      when :d8
-        board[coord] = Queen.new(:black, icon, coord)
-      when :e8
-        board[coord] = King.new(:black, icon, coord)
-      end
-    end
+    board.keys.filter_map { |coord| board[coord] = Pawn.new(:white, :classic, coord) if coord[1] == 2 }
     board
   end
 
@@ -92,4 +69,4 @@ class Board
 end
 
 player = Player.new(1, :white)
-p Board.new.move_piece(player)
+p Board.new.board

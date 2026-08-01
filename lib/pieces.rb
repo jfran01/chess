be993b74e998928200762e3d16a576df11fc6a24
Juliette <211356692+jfran01@users.noreply.github.com>
2026-08-01 16:42:37 +0100

    allow negative straight sliding

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -8,13 +8,6 @@ class Piece
   def initialize(player, _icon)
     @player = player
   end
-
-  def legal_move?(square_to)
-    square_to = convert_coords(square_to)
-    return true if @adj_coordinates.include?(square_to)
-
-    false
-  end
 end
 
 class Pawn < Piece
@@ -63,6 +56,8 @@ class Knight < Piece
 end
 
 class Bishop < Piece
+  include Slideable
+
   ICONS = { classic: { white: '♗', black: '♝' },
             letters: { white: 'WB', black: 'BB' } }.freeze
 
@@ -75,6 +70,8 @@ class Bishop < Piece
 end
 
 class Queen < Piece
+  include Slideable
+
   ICONS = { classic: { white: '♕', black: '♛' },
             letters: { white: 'WQ', black: 'BQ' } }.freeze
 
@@ -98,5 +95,7 @@ class King < Piece
   end
 end
 
-p Rook.new(:white, :classic).slide_straight([3, 7], [6, 5])
-p Rook.new(:white, :classic).slide_straight([1, 1], [1, 1])
+bishop = Bishop.new(:white, :classic)
+rook = Rook.new(:white, :classic)
+p bishop.slide_diagonal([1, 1], [4, 4])
+p rook.slide_straight([2, 6], [2, 1])

81c8752276ed6fb5a913a592910929d1c351ba42
Juliette <211356692+jfran01@users.noreply.github.com>
2026-08-04 16:30:07 +0100

    check for attack by bishop or queen

--- lib/check.rb
+++ lib/check.rb
@@ -16,15 +16,29 @@ module Check
     false
   end
 
-  def sliding_attack?(king_coord, king)
-    straight_slider_coords = []
+  def straight_attack?(king_coord, king)
+    straight_coords = []
     board.each do |coord, piece|
       if !piece.nil? && piece.player != king.player && (piece.is_a?(Rook) || piece.is_a?(Queen))
         straight_slider_coords << coord
       end
     end
-    straight_slider_coords.each do |coord|
+    straight_coords.each do |coord|
       slide_through = king.slide_straight(king_coord, coord)
+      return true if slide_through && slide_through.none? { |through| board[through] } # rubocop: disable Style/SafeNavigation
+    end
+    false
+  end
+
+  def diagonal_attack?(king_coord, king)
+    diagonal_coords = []
+    board.each do |coord, piece|
+      if !piece.nil? && piece.player != king.player && (piece.is_a?(Bishop) || piece.is_a?(Queen))
+        diagnal_coords << coord
+      end
+    end
+    diagonal_coords.each do |coord|
+      slide_through = king.slide_diagonal(king_coord, coord)
       return true if slide_through && slide_through.none? { |through| board[through] } # rubocop: disable Style/SafeNavigation
     end
     false

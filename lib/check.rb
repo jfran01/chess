da43a142df926e7e52c8978e7a543bb5d9dfda85
Juliette <211356692+jfran01@users.noreply.github.com>
2026-08-12 22:38:58 +0100

    can any pawns legally move

--- lib/check.rb
+++ lib/check.rb
@@ -140,11 +140,13 @@ end
 module Stalemate
   def stalemate?(king_colour)
     # occurs when there are no legal moves that the player of that colour can make
-    return true if king_or_queen_can_move?(king_colour)
-    return true if knight_can_move?(king_colour)
-    return true if sliding_piece_can_move?(king_colour)
+    return false if king_or_queen_can_move?(king_colour)
+    return false if knight_can_move?(king_colour)
+    return false if sliding_piece_can_move?(king_colour, Rook, [[1, 0], [-1, 0], [0, 1], [0, -1]])
+    return false if sliding_piece_can_move?(king_colour, Bishop, [[1, 1], [1, -1], [-1, -1], [-1, 1]])
+    return false if pawn_can_move?(king_colour)
 
-    false
+    true
   end
 
   # can king move?
@@ -152,26 +154,38 @@ module Stalemate
     # king_attacks map checks all immediate squares, a queen must be able to legally move to an immediate square
     [King, Queen].any? do |piece_type|
       piece_coord = find_piece_coords(piece_type, player_colour)[0]
-      king_attacks[piece_coord].any? do |coord|
-        legal_move?(piece_coord, coord)
+      king_attacks[piece_coord].any? do |move_to|
+        legal_move?(piece_coord, move_to, player_colour)
       end
     end
   end
 
   # can any knights move?
   def knight_can_move?(player_colour)
-    find_piece_coords(Knight, player_colour).any? do |coord|
-      knight_attacks[coord].any? { |coord| available_square(coord, player_colour) }
+    find_piece_coords(Knight, player_colour).any? do |knight_coord|
+      knight_attacks[knight_coord].any? { |move_to| legal_move?(knight_coord, move_to, player_colour) }
     end
   end
 
   # can any sliding pieces move?
   def sliding_piece_can_move?(player_colour, piece_type, offsets)
+    find_piece_coords(piece_type, player_colour).any? do |piece_coord|
+      adj_coords = offsets.map { |dx, dy| [piece_coord[0] + dx, piece_coord[1] + dy] }
+      adj_coords.select! { |x, y| x.between?(1, 8) && y.between?(1, 8) }
+      adj_coords.any? { |move_to| legal_move?(piece_coord, move_to, player_colour) }
+    end
   end
 
   # can any pawns move?
-  # is there an available square- either nil or occupied by an enemy piece?
-  def available_square(coord, player_colour)
-    board[coord].nil? || board[coord].player != player_colour
+  def pawn_can_move?(player_colour)
+    find_piece_coords(Pawn, player_colour).any? do |pawn_coord|
+      move_to = pawn_coord
+      move_to[1] += if player_colour == :white
+                      1
+                    else
+                      -1
+                    end
+      !board[move_to].nil? || board[move_to] != player_colour
+    end
   end
 end

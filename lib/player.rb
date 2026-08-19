a547efa953f205cf170ec4ca7a85adea4cfb65fb
Juliette <211356692+jfran01@users.noreply.github.com>
2026-08-19 22:04:13 +0100

    write custom gets method

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Player
   attr_reader :colour, :id
 
@@ -11,7 +9,7 @@ class Player
 
   def move_from
     puts 'Enter the square you would like to move from:'
-    from_coord = convert_coord(gets.chomp)
+    from_coord = convert_coord(get_input.chomp)
     return from_coord if from_coord.all? { |num| num.is_a?(Numeric) && num.between?(1, 8) }
 
     puts 'Those coordinates are not in range, please enter a valid letter + number combination'
@@ -20,7 +18,7 @@ class Player
 
   def move_to
     puts 'Enter the square you would like to move to:'
-    to_coord = convert_coord(gets.chomp)
+    to_coord = convert_coord(get_input.chomp)
     return to_coord if to_coord.all? { |num| num.between?(1, 8) }
 
     puts 'Those coordinates are not in range, please enter a valid letter + number combination'
@@ -41,5 +39,29 @@ class Player
     coord[0] = ALPHABET_CONVERTER[coord[0]]
     coord[1] = coord[1].to_i
     coord
+  end
+
+  def get_input
+    input = ''
+    loop do
+      char = STDIN.getch
+
+      case char
+      when "\cs"
+        print 'ctrl + s'
+        return
+      when "\cq"
+        print 'ctrl + q'
+      when "\r"
+        puts
+        return input
+      when "\u007F"
+        input.chop!
+        print "\b \b"
+      else
+        print char
+        input << char
+      end
+    end
   end
 end

fe94d1854d56890e8c0824f2c5cf64edc89ea43d
Juliette <211356692+jfran01@users.noreply.github.com>
2026-08-20 18:01:00 +0100

    fetch a file by retrieving filename from user

--- lib/modules/save_game.rb
+++ lib/modules/save_game.rb
@@ -7,16 +7,24 @@ module SaveGame
     filename = "#{players[0].id}_vs_#{players[1].id}"
     save_data = {
       board: board,
-      players: players,
-      filename: filename
+      players: players
     }
     save_path = File.join('saved_games', filename)
     File.write(save_path, YAML.dump(save_data))
   end
 
   def self.from_saved_data
+    filename = fetch_saved_data
+    filename = File.join('saved_games', filename)
+    game_data = YAML.load_file(filename)
+    File.delete(filename)
+    game_data
   end
 
   def self.fetch_saved_data
+    games = Dir.children('saved_games')
+    puts "You have #{games.size} games saved. Choose one:"
+    games.each_with_index { |filename, idx| puts "\e[1m(#{idx + 1})\e[0m #{filename}" }
+    games[gets.chomp.to_i - 1]
   end
 end

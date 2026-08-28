2e72d69cbe7708d82c838e855818e998fec3cdbf
Juliette <211356692+jfran01@users.noreply.github.com>
2026-08-28 10:53:28 +0100

    check? returns number of checking pieces or false (if none)

--- lib/modules/end_of_game.rb
+++ lib/modules/end_of_game.rb
@@ -1,8 +1,11 @@
 module Check
   def check?(king_colour, king_coords = find_piece_coords(King, king_colour)[0])
-    return true if attack_by_pawn?(king_colour, king_coords)
-    return true if attack_by_knight?(king_colour, king_coords)
-    return true if attack_by_sliding_piece?(king_colour, king_coords)
+    checking_pieces = 0
+    checking_pieces += attack_by_pawn?(king_colour, king_coords).size
+    checking_pieces += attack_by_knight?(king_colour, king_coords).size
+    checking_pieces += attack_by_sliding_piece?(king_colour, king_coords).size
+
+    return checking_pieces unless checking_pieces.zero?
 
     false
   end
@@ -17,16 +20,16 @@ module Check
                   else
                     black_pawn_attacks[coords]
                   end
-    pawn_coords.any? { |coord| board[coord].is_a?(Pawn) && board[coord].player != colour }
+    pawn_coords.select { |coord| board[coord].is_a?(Pawn) && board[coord].player != colour }
   end
 
   def attack_by_knight?(colour, coords)
-    knight_attacks[coords].any? { |coord| board[coord].is_a?(Knight) && board[coord].player != colour }
+    knight_attacks[coords].select { |coord| board[coord].is_a?(Knight) && board[coord].player != colour }
   end
 
   def attack_by_sliding_piece?(colour, king_coords)
     enemy_sliding_pieces = enemy_sliding_piece_coords(colour)
-    enemy_sliding_pieces.any? do |enemy_coord|
+    enemy_sliding_pieces.select do |enemy_coord|
       legal_slide?(board[enemy_coord], king_coords, enemy_coord)
     end
   end
@@ -50,6 +53,9 @@ module Checkmate
   include Check
   def avoid_checkmate(king_colour)
     king_coords = find_piece_coords(King, king_colour)&.first
+    return true if escape_check?(king_coords)
+
+    false
   end
 
   def escape_check?(king_coords)
